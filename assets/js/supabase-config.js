82bbf0a39fcef0dab68e81fe0cd69c860dfd1ea2
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-03-02 19:18:48 +0530

    Fixed Bugs

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -2,6 +2,5 @@
 // Example:
 // window.SUPABASE_URL = 'https://YOUR-PROJECT.supabase.co';
 // window.SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
-import os
-window.SUPABASE_URL = os.environ.get("URL");
-window.SUPABASE_ANON_KEY = os.environ.get("API_KEY");
+window.SUPABASE_URL = 'https://ncxduxhawivknhzvbrmo.supabase.co';
+window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jeGR1eGhhd2l2a25oenZicm1vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzIzNDU0OTQsImV4cCI6MjA4NzkyMTQ5NH0.Nti1thsB_0L41V6qI-4t8fmASendh_tYtqVq4xYRsA4';
