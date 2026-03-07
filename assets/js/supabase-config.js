296ed631665da169c5b1c8ecf0c645e09226e685
Ahmad Faraz <ahmadxfaraz@gmail.com>
2026-03-07 21:10:25 +0530

    Fix signup email redirect

--- assets/js/supabase-config.js
+++ assets/js/supabase-config.js
@@ -2,9 +2,9 @@
 // Example:
 // window.SUPABASE_URL = 'https://YOUR-PROJECT.supabase.co';
 // window.SUPABASE_ANON_KEY = 'YOUR_ANON_KEY';
-// window.SUPABASE_AUTH_REDIRECT_URL = 'http://localhost:5500/login.html';
-// window.SUPABASE_SIGNUP_REDIRECT_URL = 'http://localhost:5500/login.html';
+// window.SUPABASE_AUTH_REDIRECT_URL = 'https://ahmadxfaraz.github.io/Project-Eigen/login.html';
+// window.SUPABASE_SIGNUP_REDIRECT_URL = 'https://ahmadxfaraz.github.io/Project-Eigen/login.html';
 window.SUPABASE_URL = 'https://ncxduxhawivknhzvbrmo.supabase.co';
 window.SUPABASE_ANON_KEY = 'sb_publishable_Ldj23JGsOsr7CQCx5HSNgA_U7LOHchw';
-window.SUPABASE_AUTH_REDIRECT_URL = 'http://localhost:5500/login.html';
-window.SUPABASE_SIGNUP_REDIRECT_URL = 'http://localhost:5500/login.html';
+window.SUPABASE_AUTH_REDIRECT_URL = 'https://ahmadxfaraz.github.io/Project-Eigen/login.html';
+window.SUPABASE_SIGNUP_REDIRECT_URL = 'https://ahmadxfaraz.github.io/Project-Eigen/login.html';
